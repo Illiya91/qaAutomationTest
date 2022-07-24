8bc2a7b1f07be03d95fcece8734dd68c6b6d332f
Illiya91 <fortune.illiya@gmail.com>
2022-07-24 22:27:11 +0300

    My home work 1

---
+++ src/HomeTask3.java
@@ -0,0 +1,8 @@
+//Incompatible types: possible lossy conversion from int to byte
+public class HomeTask3{
+    public static void main(String[] args) {
+        byte a = 1000;
+        System.out.println(a);
+    }
+}
+////variable type 'Byte' problem
---
+++ src/HomeTask6.java
@@ -0,0 +1,10 @@
+public class HomeTask6{
+        public static void main(String[] args) {
+                final int a = 1000;
+                final int a = 400;
+                System.out.println(a);
+                System.out.println(a);
+        }
+}
+
+//Змінна  вже визначена в методі main(java.lang.String

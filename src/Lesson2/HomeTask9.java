4951e8b730e171ffe8c100f8b71d060cb2da9ff1
Illiya91 <fortune.illiya@gmail.com>
2022-08-01 19:15:22 +0300

    My home work 2

--- src/Lesson2/HomeTask9.java
+++ src/Lesson2/HomeTask9.java
@@ -10,20 +10,16 @@ public class HomeTask9 {
         }
         for (int out = array.length - 1; out >= 1; out--) {  //Внешний цикл
             for (int in = 0; in < out; in++) {       //Внутренний цикл
-                if (array[in] > array[in + 1]) {              //Если порядок элементов нарушен
+                if (array[in] < array[in + 1]) {              //Если порядок элементов нарушен
                     int dummy = array[in];
                     array[in] = array[in + 1];
                     array[in + 1] = dummy;
-
                 }
-
-
             }
         }
         for (int i = 0; i < array.length; i++){
             System.out.println(array[i]);
         }
     }
-
 }
 

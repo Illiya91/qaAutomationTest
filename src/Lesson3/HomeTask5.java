ea9ed2bdad94e659ec662130dcad586a68132e2f
Illiya91 <fortune.illiya@gmail.com>
2022-08-26 01:14:51 +0300

    My home work 4

--- src/Lesson3/HomeTask5.java
+++ src/Lesson3/HomeTask5.java
@@ -4,11 +4,17 @@
 package Lesson3;
 
 public class HomeTask5 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         String a = "hop";
         String b = "hello";
-        if (b != a) {
+        int len1 = a.length();
+        int len2 = b.length();
+        if (len1 > len2) {
+            System.out.println("error");
+        }
+            else {
             System.out.println(a + " " + b + " " + a);
+            }
         }
     }
-}
